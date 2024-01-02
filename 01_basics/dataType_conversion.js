d420130a004ec659c3d5502af84ce0788af4f956
Surya Ghosh <suryaghoshchemistry2k21@gmail.com>
2024-01-02 18:01:16 +0000

    dataType Conversion

---
+++ 01_basics/dataType_conversion.js
@@ -0,0 +1,31 @@
+let Score = true;
+
+console.log(typeof Score);
+console.log(typeof Score);
+
+let score = Number(Score);
+
+console.log(typeof score);
+
+console.log(score);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false; "surya" => true
+
+
+let someNumber  = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// 33 => "33"
