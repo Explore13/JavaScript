d81bd2b99cfedcbaabab7f3377409f5a6105d172
Surya Ghosh <suryaghoshchemistry2k21@gmail.com>
2024-01-02 17:47:27 +0000

    dataType added

---
+++ 01_basics/dataType.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hare krishna"); we are using nodejs, not browser
+
+console.log(3 + 3);
+console.log("surya");
+
+let name = "surya";
+let age = 20;
+let isLoggedIn = false;
+
+// number => 2 to the power 53
+// bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof null); //object
+console.log(typeof undefined); // undefined
