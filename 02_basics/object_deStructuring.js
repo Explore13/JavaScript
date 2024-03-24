b7aa05f78c2f635469d025edf2ad7302e85496c2
Surya Ghosh <suryaghoshchemistry2k21@gmail.com>
2024-03-24 20:38:08 +0000

    new object files added

---
+++ 02_basics/object_deStructuring.js
@@ -0,0 +1,28 @@
+const course = {
+    name : "js in hindi",
+    price : "999",
+    courseInstructor : "hitesh"
+}
+
+const {courseInstructor: instructor} = course;
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+
+// const navBar = ({company}) => {
+
+// }
+
+// navBar(company = "surya")
+
+
+/* API */
+
+// Values comes in Json Format
+
+// {
+//     // "key" : "value"
+//     "fullName":"surya",
+//     "money":"1000"
+// };
